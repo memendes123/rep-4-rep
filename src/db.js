fc1b6baaad96b9ade8a2e9166245bcc19456d1b8
H4RD <84364088+memendes123@users.noreply.github.com>
2024-06-04 19:34:16 +0100

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,63 +1,35 @@
-import sqlite3 from 'sqlite3'
-import { open } from 'sqlite'
-
-class dbWrapper {
-    db;
-
-    constructor() {
-    }
-
-    async init() {
-        this.db = await open({
-            filename: './steamprofiles.db',
-            driver: sqlite3.Database
-        })
-
-        await this.createProfilesTable()
-    }
-
-    async createProfilesTable() {
-        await this.db.exec(
-            `CREATE TABLE IF NOT EXISTS steamprofile (
-                id integer PRIMARY KEY AUTOINCREMENT,
-                username varchar,
-                password varchar,
-                steamId varchar UNIQUE,
-                cookies text,
-                lastComment datetime
-            )`
-        )
-    }
-
-    async addOrUpdateProfile(username, password, steamId, cookies) {
-        const result = await this.db.run('INSERT OR REPLACE INTO steamprofile (username, password, steamId, cookies) VALUES (?, ?, ?, ?)', [
-            username,
-            password,
-            steamId,
-            JSON.stringify(cookies)
-        ])
-        return result
-    }
-
-    async removeProfile(username) {
-        const result = await this.db.run('DELETE FROM steamprofile WHERE username = ?', [
-            username
-        ])
-        return result
-    }
-
-    async getAllProfiles() {
-        const result = await this.db.all('SELECT * FROM steamprofile')
-        return result
-    }
-
-    async updateLastComment(steamId) {
-        const result = await this.db.run(`UPDATE steamprofile SET lastComment=DATETIME('now', 'localtime') WHERE steamId = ?`, [
-            steamId
-        ])
-        return result
-    }
+async createProfilesTable() {
+    await this.db.exec(
+        `CREATE TABLE IF NOT EXISTS steamprofile (
+            id integer PRIMARY KEY AUTOINCREMENT,
+            username varchar,
+            password varchar,
+            steamId varchar UNIQUE,
+            cookies text,
+            lastComment datetime
+        )`
+    )
 }
 
-const instance = new dbWrapper()
-export {instance as default}
+async addOrUpdateProfile(username, password, steamId, cookies) {
+    const result = await this.db.run('INSERT OR REPLACE INTO steamprofile (username, password, steamId, cookies) VALUES (?, ?, ?, ?)', [
+        username,
+        password,
+        steamId,
+        JSON.stringify(cookies)
+    ])
+    return result
+}
+
+async updateCookies(steamId, cookies) {
+    const result = await this.db.run('UPDATE steamprofile SET cookies = ? WHERE steamId = ?', [
+        JSON.stringify(cookies),
+        steamId
+    ])
+    return result
+}
+
+async getProfileBySteamId(steamId) {
+    const result = await this.db.get('SELECT * FROM steamprofile WHERE steamId = ?', [steamId])
+    return result
+}
