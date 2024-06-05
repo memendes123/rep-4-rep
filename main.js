cb7b03e74091c561ffb660fc62ccfe3a6ee93969
H4RD <84364088+memendes123@users.noreply.github.com>
2024-06-05 16:40:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 import steamBot from './src/steamBot.js';
 import db from './src/db.js';
 import api from './src/api.js';
-import { log, showAllProfiles, addProfileSetup, authAllProfiles, removeProfile, autoRun, addProfilesFromFile, addProfilesAndRun, checkAndSyncProfiles } from './src/util.js';
+import { log, showAllProfiles, addProfileSetup, authAllProfiles, removeProfile, autoRun, addProfilesFromFile, addProfilesAndRun, checkCommentAvailability } from './src/util.js';
 
 var argv = parseArgs(process.argv.slice(2));
 await db.init();
@@ -30,6 +30,9 @@ await db.init();
 
 // check and sync profiles:
 //  --check-and-sync-profiles
+
+// check comment availability:
+// --check-comment-availability
 
 if (argv['run']) {
     await autoRun();
@@ -62,6 +65,10 @@ if (argv['add-profiles-and-run']) {
 
 if (argv['check-and-sync-profiles']) {
     await checkAndSyncProfiles();
+}
+
+if (argv['check-comment-availability']) {
+    await checkCommentAvailability();
 }
 
 process.exit();
